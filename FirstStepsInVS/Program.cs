4880fd889eeb69b968c98a44e7f9f164b17993a0
Milou <Milou@DESKTOP-R3P5NON>
2017-02-14 01:40:17 +0100

    Validayion des Modifications!!!

--- FirstStepsInVS/Program.cs
+++ FirstStepsInVS/Program.cs
@@ -10,6 +10,12 @@ namespace FirstStepsInVS
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("mais alors .....!!");
+
+            Console.WriteLine("c'est ok,je suis dans la matrice !!! ha ha ha !!!!");
+            Console.WriteLine("pas simple de faire fonctionner githuB dans VisualStudio...");
+            Console.WriteLine("c'est à en devenir fou fou fooooouuuuuuuu!!!!!");
+            Console.WriteLine("enfin ,je retrouve mon projet VisuStu dans CitHub,\n ce qui me permet de faire des Push n Commit...");
         }
     }
 }
